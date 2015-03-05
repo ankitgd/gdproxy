358d3ad0ae21c6da55f546860062bf8a017b6e4e
Ankit Patel <ankit@greatdanedata.com>
2015-03-05 21:32:07 +0000

    Add Req/Resp to example

--- examples/main.go
+++ examples/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/ankitgd/gdproxy"
 	"log"
 	"flag"
+	//"fmt"
 	"net/http"
 )
 
@@ -11,7 +12,19 @@ func main() {
 	verbose := flag.Bool("v", false, "should every proxy request be logged to stdout")
 	addr := flag.String("addr", ":8080", "proxy listen address")
 	flag.Parse()
+
 	proxy := gdproxy.NewProxyHttpServer()
 	proxy.Verbose = *verbose
+
+	proxy.OnRequest().DoFunc(func(r *http.Request,ctx *gdproxy.ProxyCtx) (*http.Request, *http.Response){
+		r.Header.Set("X-GoProxy","1")
+		return r, nil
+	})
+
+	proxy.OnResponse().DoFunc(func(r *http.Response, ctx *gdproxy.ProxyCtx)*http.Response{
+		println(ctx.Req.Host,"->",r.Header.Get("Content-Type"))
+		return r
+	})
+
 	log.Fatal(http.ListenAndServe(*addr, proxy))
 }
